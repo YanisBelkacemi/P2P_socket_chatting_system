275545484dffe8785c0df1d1a4921d1bfef13b9a
YanisBelkacemi <108769998+YanisBelkacemi@users.noreply.github.com>
2025-02-22 19:13:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,7 @@
 import socket
 import threading
+
+
+def server(ip = "0.0.0.0" , port = 60):
+    server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    server_socket.bind(ip, port)
